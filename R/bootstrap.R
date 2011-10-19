0dde98cbdd916b821f5cd74d6f32dc1f78cc1106
Yves Rosseel <yrosseel@gmail.com>
2011-10-19 14:01:03 +0200

    added the bootstrapLRT() function to compare two (nested) models h0 and h1 using the parametric or bollen.stine bootstrap

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -87,15 +87,154 @@ lavaanBootStatistic <- function(data=NULL, boot.idx=NULL, start=NULL,
 }
 
 
-# Notes: - faulty runs are REMOVED!
-#        - default R=1000
-#
-#        - 3 types: 1) ordinary (=nonparametric), 
-#                   2) bollen.stine (transform data under H0 + nonparametric)
-#                   3) parametric
+bootstrapLRT.internal <- function(model1=NULL, model2=NULL, 
+                                  sample=NULL, options=NULL,
+                                  data=NULL, R=1000L,
+                                  type="bollen.stine", 
+                                  verbose=FALSE,
+                                  max.iter=1000L) {
+
+    # checks
+    stopifnot(!is.null(model1), !is.null(model2),
+              !is.null(sample), !is.null(options), 
+              !is.null(data), 
+              type %in% c("bollen.stine", "parametric"))
+
+    # prepare
+    LRT <- rep(as.numeric(NA), R)
+
+    # bollen.stine or parametric: we need the Sigma.hat values
+    Sigma.hat <- computeSigmaHat(model1)
+    Mu.hat <- computeMuHat(model1) 
+
+    # run bootstraps
+    error.idx <- integer(0)
+    for(b in 1:R) {
+        if(type == "bollen.stine") {
+            # take a bootstrap sample
+            N <- sample@ntotal
+            boot.idx <- sample(x=N, size=N, replace=TRUE)
+        } else {
+            # parametric!
+            boot.idx <- NULL
+        }
+
+        # verbose
+        if(verbose) cat("  ... bootstrap draw number: ", b, "\n")
+
+        # create data
+        bootSample <-
+            try( Sample(data          = data,
+                        group         = options$group,
+                        sample.cov    = NULL,
+                        sample.mean   = NULL,
+                        sample.nobs   = NULL,
+                        std.ov        = FALSE,
+                        boot.idx      = boot.idx,
+                        model.cov     = Sigma.hat,
+                        model.mean    = Mu.hat,
+
+                        ov.names      = sample@ov.names,
+                        data.type     = "full",
+                        ngroups       = sample@ngroups,
+                        group.label   = sample@group.label,
+                        estimator     = options$estimator,
+                        likelihood    = options$likelihood,
+                        mimic         = options$mimic,
+                        meanstructure = options$meanstructure,
+                        missing       = options$missing,
+                        warn          = options$warn,
+                        verbose       = options$verbose) )
+        if(inherits(bootSample, "try-error")) {
+            if(verbose) cat("     FAILED: creating sample statistics\n")
+            error.idx <- c(error.idx, b)
+            next
+        }
+
+        # estimate model 1
+        if(verbose) cat("  ... ... model 1: ")
+        start1 <- getModelParameters(model1, type="free")
+        lavaanModel1 <- setModelParameters(model1, x = start1)
+        # switch of verbose in estimateModel
+        verbose.old <- options$verbose; options$verbose <- FALSE
+        x1 <- try( estimateModel(lavaanModel1,
+                                 sample  = bootSample,
+                                 options = options) )
+        options$verbose <- verbose.old
+        if(inherits(x1, "try-error")) {
+            if(verbose) cat("     FAILED: in estimation\n")
+            error.idx <- c(error.idx, b)
+            next
+        } else if(!attr(x1, "converged")) {
+            if(verbose) cat("     FAILED: no convergence\n")
+            error.idx <- c(error.idx, b)
+            next
+        } 
+        if(verbose) cat("     ok -- niter = ", attr(x1, "iterations"),
+                        " fx = ", attr(x1, "fx"), "\n")
+
+        # estimate model 2
+        if(verbose) cat("  ... ... model 2: ")
+        start2 <- getModelParameters(model2, type="free")
+        lavaanModel2 <- setModelParameters(model2, x = start2)
+        # switch of verbose in estimateModel
+        verbose.old <- options$verbose; options$verbose <- FALSE
+        x2 <- try( estimateModel(lavaanModel2,
+                                 sample  = bootSample,
+                                 options = options) )
+        options$verbose <- verbose.old
+        if(inherits(x2, "try-error")) {
+            if(verbose) cat("     FAILED: in estimation\n")
+            error.idx <- c(error.idx, b)
+            next
+        } else if(!attr(x2, "converged")) {
+            if(verbose) cat("     FAILED: no convergence\n")
+            error.idx <- c(error.idx, b)
+            next
+        }
+        if(verbose) cat("     ok -- niter = ", attr(x2, "iterations"),
+                        " fx = ", attr(x2, "fx"), "\n")
+
+
+        # store LRT
+        NFAC <- 2 * unlist(sample@nobs)
+        if(options$estimator == "ML" && options$likelihood == "wishart") {
+            # first divide by two
+            NFAC <- NFAC / 2
+            NFAC <- NFAC - 1
+            NFAC <- NFAC * 2
+        }
+        # model 1
+        fx.group1 <- attr(attr(x1, "fx"), "fx.group")
+        chisq.group1 <- fx.group1 * NFAC
+        chisq.group1[which(chisq.group1 < 0)] <- 0.0
+        chisq1 <- sum(chisq.group1)
+
+        # model 2
+        fx.group2 <- attr(attr(x2, "fx"), "fx.group")
+        chisq.group2 <- fx.group2 * NFAC
+        chisq.group2[which(chisq.group2 < 0)] <- 0.0
+        chisq2 <- sum(chisq.group2)
+
+        LRT[b] <- abs(chisq1 - chisq2)
+    }
+
+    # handle errors
+    if(length(error.idx) > 0L) {
+        warning("lavaan WARNING: only ", (R-length(error.idx)), " bootstrap draws were successful")
+        LRT <- LRT[-error.idx]
+        attr(LRT, "error.idx") <- error.idx
+    } else {
+        if(verbose) cat("Number of successful bootstrap draws:", 
+                        (R - length(error.idx)), "\n")
+    }
+
+    LRT
+}
+
 bootstrap.internal <- function(model=NULL, sample=NULL, options=NULL,
                                data=NULL, R=1000L,
-                               type="ordinary", 
+                               type="ordinary",
                                verbose=FALSE,
                                coef=TRUE,
                                test=FALSE,
@@ -104,9 +243,9 @@ bootstrap.internal <- function(model=NULL, sample=NULL, options=NULL,
                                max.iter=1000L) {
 
     # checks
-    stopifnot(!is.null(model), !is.null(sample), !is.null(options), 
-              !is.null(data), 
-              type %in% c("nonparametric", "ordinary", 
+    stopifnot(!is.null(model), !is.null(sample), !is.null(options),
+              !is.null(data),
+              type %in% c("nonparametric", "ordinary",
                           "bollen.stine", "parametric"))
     if(type == "nonparametric") type <- "ordinary"
 
@@ -117,7 +256,7 @@ bootstrap.internal <- function(model=NULL, sample=NULL, options=NULL,
     COEF <- TEST <- NULL
     if(coef) {
         COEF <- matrix(NA, R, npar)
-    } 
+    }
     if(test) {
         TEST <- rep(as.numeric(NA), R)
     }
@@ -125,7 +264,7 @@ bootstrap.internal <- function(model=NULL, sample=NULL, options=NULL,
     # bollen.stine or parametric: we need the Sigma.hat values
     if(type == "bollen.stine" || type == "parametric") {
         Sigma.hat <- computeSigmaHat(model)
-        Mu.hat <- computeMuHat(model) 
+        Mu.hat <- computeMuHat(model)
     } else {
         Sigma.hat <- NULL
         Mu.hat <- NULL
@@ -149,8 +288,8 @@ bootstrap.internal <- function(model=NULL, sample=NULL, options=NULL,
                                  Sigma.hat=Sigma.hat, Mu.hat=Mu.hat, test=test)
 
         # catch faulty run
-        if(any(is.na(x))) { 
-            error.idx <- c(error.idx, b)    
+        if(any(is.na(x))) {
+            error.idx <- c(error.idx, b)
             next
         }
 
@@ -180,14 +319,14 @@ bootstrap.internal <- function(model=NULL, sample=NULL, options=NULL,
     if(length(error.idx) > 0L) {
         warning("lavaan WARNING: only ", (R-length(error.idx)), " bootstrap draws were successful")
         COEF <- COEF[-error.idx,,drop=FALSE]
-        attr(COEF, "error.idx") <- error.idx
+        #attr(COEF, "error.idx") <- error.idx
 
         if(test) {
             TEST <- TEST[-error.idx]
         }
 
     } else {
-        if(verbose) cat("Number of successful bootstrap draws:", 
+        if(verbose) cat("Number of successful bootstrap draws:",
                         (R - length(error.idx)), "\n")
     }
 
@@ -225,6 +364,20 @@ bootstrapLavaan <- function(object, data=NULL, R=1000, type="ordinary",
 
     out
 }
+
+bootstrapLRT <- function(h0, h1, data=NULL, R=1000, 
+                         type="bollen.stine", verbose=FALSE) {
+
+    out <- bootstrapLRT.internal(model1=h0@Model,
+                                 model2=h1@Model,
+                                 sample=h0@Sample,
+                                 options=h0@Options,
+                                 data=data, R=R,
+                                 verbose=verbose,
+                                 type=type)
+    out
+}
+
 
 # using the 'boot' package
 bootLavaan <- function(object, data=NULL, R=1000, ..., verbose=FALSE) {
