197a1330f08de9c0f104c25ddebe68431fa5b411
Yves Rosseel <yrosseel@gmail.com>
2012-03-05 11:49:00 +0100

    changed check for linear constraints

--- R/utils-constraints.R
+++ R/utils-constraints.R
@@ -1,18 +1,24 @@
 # FIXME: is there a more elegant/robust way to do this??
 checkLinearConstraints <- function(model) {
 
-     # seed = zero vector
-     A.ceq <- t(jacobian(func=model@ceq.function, x=rep(0,model@nx.free)))
-     A.cin <- t(jacobian(func=model@cin.function, x=rep(0,model@nx.free)))
+     # seed 1: rnorm
+     A.ceq <- A.cin <- matrix(0, model@nx.free, 0)
+     if(!is.null(body(model@ceq.function)))
+         A.ceq <- t(jacobian(func=model@ceq.function, x=rnorm(model@nx.free)))
+     if(!is.null(body(model@cin.function)))
+         A.cin <- t(jacobian(func=model@cin.function, x=rnorm(model@nx.free)))
      A0 <- cbind(A.ceq, A.cin)    
 
-     # seed = unit vector
-     A.ceq <- t(jacobian(func=model@ceq.function, x=rep(1,model@nx.free)))
-     A.cin <- t(jacobian(func=model@cin.function, x=rep(1,model@nx.free)))
+     # seed 2: rnorm
+     A.ceq <- A.cin <- matrix(0, model@nx.free, 0)
+     if(!is.null(body(model@ceq.function)))
+         A.ceq <- t(jacobian(func=model@ceq.function, x=rnorm(model@nx.free)))
+     if(!is.null(body(model@cin.function)))
+         A.cin <- t(jacobian(func=model@cin.function, x=rnorm(model@nx.free)))
      A1 <- cbind(A.ceq, A.cin)
 
-     A0minA1 <- all.equal(A0,A1)
-     if(is.logical(A0minA1) && A0minA1 == TRUE) 
+     A0minA1 <- all.equal(A0, A1)
+     if(is.logical(A0minA1) && A0minA1 == TRUE)
          return(TRUE)
      else
          return(FALSE)
