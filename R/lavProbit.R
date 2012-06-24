ce5607f0bed4f0ab318fe500980553b0bec8b5b8
Yves Rosseel <yrosseel@gmail.com>
2012-06-24 17:35:50 +0200

    some clean up in lavRefProbit()

--- R/lavProbit.R
+++ R/lavProbit.R
@@ -32,31 +32,33 @@ lavRefProbit <- setRefClass("lavProbit",
 contains = "lavOptim",
 
 # fields
-fields = list( nth = "integer", nx = "integer", freq    = "integer",
-    y = "integer", Y1 = "matrix", Y2 = "matrix", X = "matrix", 
-    probits = "numeric", eta = "numeric",
-    wts = "numeric", wtpr = "numeric", offset = "numeric",
-    dpi.psi = "matrix"),
+fields = list(y = "integer", X = "matrix", 
+              nobs = "integer", nexo = "integer", nth = "integer", 
+              weights = "numeric", offset = "numeric",
+              Y1 = "matrix", Y2 = "matrix",
+              # cache:
+              # this doesn't result in better speed; but makes the code cleaner
+              z1 = "numeric", z2 = "numeric", probits = "numeric",
+              p1 = "numeric", p2 = "numeric"),
 
 # methods
 methods = list(
 
 initialize = function(y, X, weights = rep(1, length(y)),
                       offset = rep(0, length(y))) {
-    cat("this is lavProbit -- classic!\n")
     # y
-    y <<- as.integer(y); nth <<- nlevels(y) - 1L; freq <<- tabulate(y)
+    y <<- as.integer(y); nth <<- length(unique(.self$y)) - 1L
     # X
-    X <<- X; n <- nrow(X); nx <<- ncol(X); nbeta <- NCOL(X)
-
-    Y <- matrix(0, n, nth)
-    Y1 <<- col(Y) == .self$y; Y2 <<- col(Y) == (.self$y - 1L)
-
-    probits <<- numeric(length = n)
-    wts <<- weights; offset <<- offset
+    X <<- X; nobs <<- nrow(X); nexo <<- ncol(X)
+    # weights and offset
+    weights <<- weights; offset <<- offset
+    
+    # TH matrices (TRUE/FALSE)
+    Y1 <<- matrix(1:nth, nobs, nth, byrow=TRUE) == .self$y
+    Y2 <<- matrix(1:nth, nobs, nth, byrow=TRUE) == (.self$y - 1L)
 
     # set up for Optim
-    npar <<- nth + nbeta
+    npar  <<- nth + nexo
     theta <<- rep(as.numeric(NA), npar)
 },
 
@@ -64,26 +66,23 @@ initialize = function(y, X, weights = rep(1, length(y)),
 objective = function(x) {
     if(!missing(x)) theta <<- x
     th <- theta[1:nth]; TH <- c(-Inf, th, +Inf); beta <- theta[-c(1:nth)]
-    eta <<- drop(X %*% beta) + offset
-    z1 <- pmin(100,TH[y+1L]  - eta); z2 <- pmax(-100, TH[y+1L-1L] - eta)
+    eta <- drop(X %*% beta) + offset
+    z1 <<- pmin( 100, TH[y+1L   ] - eta)
+    z2 <<- pmax(-100, TH[y+1L-1L] - eta)
     probits <<- pnorm(z1) - pnorm(z2)
     if(all(probits > 0))
-        -sum(wts * log(probits))
+        -sum(weights * log(probits))
     else Inf
 },
 
 gradient = function(x) {
     if(!missing(x)) objective(x)
-    th <- theta[1:nth]; TH <- c(-Inf, th, +Inf); beta <- theta[-c(1:nth)]
-    eta <<- drop(X %*% beta) + offset
-    z1 <- pmin(100,TH[y+1L]  - eta); z2 <- pmax(-100, TH[y+1L-1L] - eta)
-    probits <<- pnorm(z1) - pnorm(z2)
-    p1 <- dnorm(z1); p2 <- dnorm(z2)
+    p1 <<- dnorm(z1); p2 <<- dnorm(z2)
     
     # beta
-    dx.beta <- (wts * (p1 - p2)/probits) %*% X
+    dx.beta <- crossprod(X, weights*(p1 - p2)/probits)
     # th
-    dx.th <- -crossprod(Y1 * p1 - Y2 * p2, wts/probits)
+    dx.th   <- crossprod(Y2*p2 - Y1*p1, weights/probits)
 
     c(dx.th, dx.beta)
 },
@@ -91,37 +90,32 @@ gradient = function(x) {
 hessian = function(x) {
     if(!missing(x)) { objective(x); gradient() }
     #cat("hessian num = \n"); print(round(numDeriv:::hessian(func=.self$objective, x=x),3))
-    th <- theta[1:nth]; TH <- c(-Inf, th, +Inf); beta <- theta[-c(1:nth)]
-    z1 <- pmin(100,TH[y+1L]  - eta); z2 <- pmax(-100, TH[y+1L-1L] - eta)
-    p1 <- dnorm(z1); p2 <- dnorm(z2)
     gnorm <- function(x) { -x * dnorm(x) }
+    wtpr <- weights/probits
 
-    wtpr <<- wts/probits
     dxb <-  X*p1 - X*p2
-    dx2.beta <- -(crossprod(X * gnorm(z1) * wtpr, X) - 
-                   crossprod(X * gnorm(z2) * wtpr, X)) +
-                 crossprod(dxb, (dxb * wtpr / probits))
+    dx2.beta <- crossprod(dxb, (dxb * wtpr / probits)) -
+                  ( crossprod(X * gnorm(z1) * wtpr, X) - 
+                    crossprod(X * gnorm(z2) * wtpr, X) )
 
     dxa <- Y1*p1 - Y2*p2
-    dx2.alpha <- -(crossprod(Y1 * gnorm(z1) * wtpr, Y1) -
-                   crossprod(Y2 * gnorm(z2) * wtpr, Y2)) +
-                  crossprod(dxa, (dxa * wtpr / probits))
+    dx2.alpha <- crossprod(dxa, (dxa * wtpr / probits)) -
+                   ( crossprod(Y1 * gnorm(z1) * wtpr, Y1) -
+                     crossprod(Y2 * gnorm(z2) * wtpr, Y2) )
 
-    dx.ab <- -(crossprod(Y1 * gnorm(z1) * wtpr, X) -
-               crossprod(Y2 * gnorm(z2) * wtpr, X)) +
-              crossprod(dxa, (dxb * wtpr / probits))
-    dx.ab <- -1*dx.ab ##??? FIXME?
+    dx.ab <- crossprod(dxa, (dxb * wtpr / probits)) -
+               ( crossprod(Y1 * gnorm(z1) * wtpr, X) -
+                 crossprod(Y2 * gnorm(z2) * wtpr, X) )
+    dx.ab <- -1*dx.ab 
 
-    H <- rbind( cbind(dx2.alpha, dx.ab),
-                cbind(t(dx.ab), dx2.beta) )
-
-    #print(round(H,3))
+    H <- rbind( cbind(dx2.alpha,   dx.ab),
+                cbind(t(dx.ab), dx2.beta)  )
 },
 
 start = function() {
     #th.start <- sort(rnorm(nth))
-    th.start <- lavaan:::unithord(freq=freq) # unconditional th's
-    beta.start <- rep(0, nx)
+    th.start <- lavaan:::unithord(freq=tabulate(y)) # unconditional th's
+    beta.start <- rep(0, nexo)
     c( th.start, beta.start )
 }
 
@@ -137,21 +131,18 @@ start = function() {
 # http://eeecon.uibk.ac.at/psychoco/2012/slides/Christensen.pdf
 # see also the 'ordinal' package
 #
-# in practice, this is only about 15-20% faster
+# in practice, this seems to be about 10-15% faster
 #
-# this function will most likely change a lot in the future
-# it is one of my first attempts to exploit the setRefClass functionality
-# in R
 lavRefProbitB <- setRefClass("lavProbitB",
 
 # inherits
 contains = "lavOptim",
 
 # fields
-fields = list( nth = "integer", nx = "integer", freq    = "integer",
+fields = list( nth = "integer", nexo = "integer", freq    = "integer",
     B1 = "matrix", B2 = "matrix", o1 = "numeric", o2 = "numeric",
     probits = "numeric", eta1 = "numeric", eta2 = "numeric",
-    wts = "numeric", wtpr = "numeric", dpi.psi = "matrix"),
+    weights = "numeric", wtpr = "numeric", dpi.psi = "matrix"),
 
 # methods
 methods = list(
@@ -160,21 +151,21 @@ initialize = function(y, X, weights = rep(1, length(y)),
                       offset = rep(0, length(y))) {
     # set up internal fields
     nth <<- nlevels(y) - 1L; freq <<- tabulate(y)
-    n <- nrow(X); nx <<- ncol(X)
+    n <- nrow(X); nexo <<- ncol(X)
     B2.tmp <- 1 * (col(matrix(0, n, nth + 1L)) == c(unclass(y)))
     o1 <<-  c( 1e5 * B2.tmp[, nth + 1L]) - offset
     o2 <<-  c(-1e5 * B2.tmp[,1L]) - offset
     B1 <<-  B2.tmp[, -(nth + 1L), drop = FALSE]
     B2 <<-  B2.tmp[, -1L, drop = FALSE]
-    nbeta <- NCOL(X) 
-    if(nbeta > 0) {
+    nexo <<- NCOL(X) 
+    if(nexo > 0) {
         B1 <<- cbind(B1, -X)
         B2 <<- cbind(B2, -X)
     }
     dimnames(B1) <<- NULL; dimnames(B2) <<- NULL
     probits <<- numeric(length = n)
-    wts <<- weights
-    npar <<- nth + nbeta
+    weights <<- weights
+    npar <<- nth + nexo
     theta <<- rep(as.numeric(NA), npar)
 },
 
@@ -183,14 +174,14 @@ objective = function(x) {
     eta1 <<- drop(B1 %*% theta) + o1; eta2 <<- drop(B2 %*% theta) + o2
     probits <<- pnorm(eta1) - pnorm(eta2)
     if(all(probits > 0))
-          -sum(wts * log(probits))
+          -sum(weights * log(probits))
     else Inf    
 },
 
 gradient = function(x) {
     if(!missing(x)) objective(x)
     p1 <- dnorm(eta1); p2 <- dnorm(eta2)
-    wtpr <<- wts/probits
+    wtpr <<- weights/probits
     dpi.psi <<- B1 * p1 - B2 * p2
     -crossprod(dpi.psi, wtpr)    
 },
@@ -205,7 +196,7 @@ hessian = function(x) {
 
 start = function() {
     th.start <- lavaan:::unithord(freq=freq) # unconditional th's
-    beta.start <- rep(0, nx)
+    beta.start <- rep(0, nexo)
     c( th.start, beta.start )
 }
 
