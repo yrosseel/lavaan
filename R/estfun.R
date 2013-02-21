807af5b566890a86a229e56ea83fbcf21316393b
Yves Rosseel <yrosseel@gmail.com>
2013-02-21 19:59:01 +0100

    add scaling argument to estfun.lavaan(); update documentation

--- R/estfun.R
+++ R/estfun.R
@@ -1,14 +1,14 @@
 # contributed by Ed Merkle (17 Jan 2013)
 # small changes by YR (12 Feb 2013) to match the results
 # of computeGradient in the multiple group case
-estfun.lavaan <- lavScores <- function(object)
+estfun.lavaan <- lavScores <- function(object, scaling=FALSE)
 {
   ## number variables/sample size
   samplestats <- object@SampleStats
   ntab <- unlist(samplestats@nobs)
-  nobs <- samplestats@ntotal
+  ntot <- samplestats@ntotal
 
-  Score.mat <- matrix(NA, nobs, length(coef(object)))
+  Score.mat <- matrix(NA, ntot, length(coef(object)))
   
   for(g in 1:samplestats@ngroups) {
     if (samplestats@ngroups > 1){
@@ -24,6 +24,7 @@ estfun.lavaan <- lavScores <- function(object)
         Mu.hat <- moments$mean
         X <- object@Data@X[[g]]
         Sigma.inv <- lavaan:::inv.chol(Sigma.hat, logdet=FALSE)
+        group.w <- (unlist(samplestats@nobs)/samplestats@ntotal)
 
         J <- matrix(1, 1L, ntab[g]) ## FIXME: needed? better maybe rowSums/colSums?
         J2 <- matrix(1, nvar, nvar)
@@ -38,7 +39,12 @@ estfun.lavaan <- lavScores <- function(object)
            function(x) lavaan:::vech(- J2 * (Sigma.inv %*% (tcrossprod(x) - Sigma.hat) %*% Sigma.inv))))
 
         scores.H1 <- cbind(dx.Mu, dx.Sigma)
-    
+        ## FIXME? Seems like we would need group.w even in the
+        ##        complete-data case:
+        ##if(scaling){
+        ##  scores.H1 <- group.w[g] * scores.H1
+        ##}
+
       } else {
         ## no mean structure
         stop("Score calculation with no mean structure is not implemented.")
@@ -58,10 +64,6 @@ estfun.lavaan <- lavScores <- function(object)
       for(p in 1:length(M)) {
         ## Data
         X <- M[[p]][["X"]]
-        ## Cov matrix of data in pattern p (divide by n, not n-1)
-        SX <- M[[p]][["SX"]]
-        ## Mean vector of data in pattern p
-        MX <- M[[p]][["MX"]]
         nobs <- M[[p]][["nobs"]]
         var.idx <- M[[p]][["var.idx"]]
         ## Which unique entries of covariance matrix are estimated?
@@ -77,32 +79,25 @@ estfun.lavaan <- lavScores <- function(object)
         Mu <- Mu.hat[var.idx]
         mean.diff <- t(t(X) - Mu %*% J)
 
-        ## Scores for group g
+        ## Scores for missing pattern p within group g
         score.mu[pat.idx==p,var.idx] <- -1 * mean.diff %*% Sigma.inv
         score.sigma[pat.idx==p,Sigma.idx] <- t(apply(mean.diff, 1L,
           function(x) lavaan:::vech(- J2 * (Sigma.inv %*% (tcrossprod(x) - Sigma.hat[var.idx,var.idx]) %*% Sigma.inv)) ) )
 
-        scores.H1 <- (group.w[g]^2) * cbind(score.mu, score.sigma)
-        ## FIXME??? Why do we need group.w[g]*group.w[g]??
+      }
 
-        ## FIXME?: On the above line in computeOmega, Yves had W.tilde
-        ##        (see below) instead of
-        ##        tcrossprod(x).  But I believe this is only needed
-        ##        when we take a gradient across a sample, vs for an
-        ##        individual observation.
-
-        ##W.tilde <- SX + tcrossprod(MX - Mu)
-        ##OMEGA[var.idx, var.idx] <-
-        ##  ( OMEGA[var.idx, var.idx] + nobs/samplestats@ntotal *
-        ##   (Sigma.inv %*%
-        ##    (W.tilde - Sigma.hat[[g]][var.idx,var.idx]) %*%
-        ##    Sigma.inv ) )
+      scores.H1 <- cbind(score.mu, score.sigma)
+      if(scaling){
+        scores.H1 <- group.w[g] * scores.H1
       }
     } # missing
     
     Delta <- lavaan:::computeDelta(object@Model)[[g]]
     wi <- object@Data@case.idx[[g]]
-    Score.mat[wi,] <- (1/nobs) * scores.H1 %*% Delta
+    Score.mat[wi,] <- -scores.H1 %*% Delta
+    if(scaling){
+      Score.mat[wi,] <- (-1/ntot) * Score.mat[wi,]
+    }
   } # g
 
   Score.mat
