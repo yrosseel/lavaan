c5b6b19438e6ca69649c36b9b518d97930f0cd7b
Yves Rosseel <yrosseel@gmail.com>
2014-01-14 11:17:10 +0100

    more file renaming

---
+++ R/01RefClass_02lavML.R
@@ -0,0 +1,43 @@
+# Maximum Likelihood optimization -- YR 10 july 2012
+
+# super class -- virtual statistical model that needs to be optimized
+lavRefML <- setRefClass("lavML",
+
+# inherits
+contains = "lavOptim",
+
+# fields
+fields = list(
+    y = "numeric",        # the (unidimensional) data
+    nobs = "integer",     # number of observations
+    weights = "numeric"   # weights
+),
+
+# methods
+methods = list(
+
+logl = function(x) {
+    if(!missing(x)) theta <<- x
+    likelihoods <- lik()
+    # FIXME: handle zero/negative/small likelihood values
+    sum(log(likelihoods), na.rm=TRUE)
+},
+
+lik = function(x) {
+    if(!missing(x)) theta <<- x
+    cat("this is dummy function\n")
+    return(rep(as.numeric(NA), nobs))
+},
+
+scores = function(x) {
+    if(!missing(x)) theta <<- x
+    cat("this is dummy function\n")
+    return(matrix(as.numeric(NA), nobs, npar))
+}, 
+
+gradient = function(x) {
+    SCORES <- scores(x)
+    apply(SCORES, 2L, sum, na.rm=TRUE)
+}
+
+))
