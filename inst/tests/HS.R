c06edb2a3299908665e0201eef548f28a2d6a019
Yves Rosseel <yrosseel@gmail.com>
2014-09-30 14:46:26 +0200

    remove inst/tests directory from lavaan package

--- inst/tests/HS.R
+++
@@ -1,9 +0,0 @@
-library(lavaan)
-
-HS.model <- ' visual  =~ x1 + x2 + x3
-              textual =~ x4 + x5 + x6
-              speed   =~ x7 + x8 + x9 '
-fit <- cfa(HS.model, data=HolzingerSwineford1939)
-
-# default extract functions
-source("common.srcR", echo = TRUE)
--- inst/tests/makeRoutsave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f *.Rout.save
-for i in *.R
-do
-  R --no-save < $i > $i'out.save' 2>&1
-done
-
