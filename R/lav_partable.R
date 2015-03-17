38bbce53ed5c8270f61e67c1f19f191c2429f20e
Yves Rosseel <yrosseel@gmail.com>
2015-03-17 19:35:34 +0100

    small fix related to the prior() modifier

--- R/lav_partable.R
+++ R/lav_partable.R
@@ -356,7 +356,9 @@ lavaanify <- lavParTable <- function(
         LIST$ustart     <- c(LIST$ustart,     rep(as.numeric(NA), length(lhs)))
         LIST$exo        <- c(LIST$exo,        rep(0L, length(lhs)) )
         LIST$label      <- c(LIST$label,      rep("",  length(lhs)) )
-        LIST$prior      <- c(LIST$prior,      rep("",  length(lhs)) )
+        if(!is.null(LIST$prior)) {
+            LIST$prior      <- c(LIST$prior,      rep("",  length(lhs)) )
+        }
         LIST$plabel     <- c(LIST$plabel,     rep("",  length(lhs)) )
         #LIST$eq.id      <- c(LIST$eq.id,      rep(0L,  length(lhs)) )
         #LIST$unco       <- c(LIST$unco,       rep(0L,  length(lhs)) )
