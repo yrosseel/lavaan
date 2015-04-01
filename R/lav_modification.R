4adf56f481a727ad452fa5a5355cc05209990225
Yves Rosseel <yrosseel@gmail.com>
2015-04-01 17:47:13 +0200

    modindices() should ignore equality constraints (for now) until we find a better way to handle them (in general)

--- R/lav_modification.R
+++ R/lav_modification.R
@@ -134,64 +134,66 @@ modindices <- function(object,
     }
 
     # correct mi's for equality constraints
-    if(length(model.idx) > 0L && object@Model@eq.constraints) {
-        # take care of equality constraints
-        # Sorbom 1989 equations 12 and 13
-        # surely this code needs some serious optimization
-        # but it seems to work for now
-        # we should use the K matrix somewhere...
-
-        K <- object@Model@eq.constraints.K
-        con.idx <- which(colSums(K != 0) > 1)
-        ncon <- length(con.idx) # # number of constraints
-
-        for(con in seq_len(ncon)) {
-
-            # what are the involved parameters (in model.idx)?
-            eq.idx <- which(K[,con.idx[con]] > 0)
-
-            for(i in 1:length(eq.idx)) {
-                # index in all.idx (only mi.fixed elements)
-                theta2.idx <- model.idx[ eq.idx[i]  ]
-                thetac.idx <- model.idx[ eq.idx[-i] ]
-                #f.idx <- free.idx[-which(free.idx == theta2.idx)]
-                f.idx <- model.idx[ -eq.idx ]
-                Q22 <- Q[f.idx, f.idx, drop=FALSE]
-
-                ivec <- as.matrix(rep(1,length(thetac.idx)))
-                C11 <- Q[thetac.idx, thetac.idx, drop=FALSE]
-                C21 <- Q[theta2.idx, thetac.idx, drop=FALSE]; C12 <- t(C21)
-                C22 <- Q[theta2.idx, theta2.idx, drop=FALSE]
-                D1  <- Q[f.idx, thetac.idx, drop=FALSE];
-                d2  <- Q[f.idx, theta2.idx, drop=FALSE]
-    
-                c11 <- t(ivec) %*% C11 %*% ivec
-                c21 <- C21 %*% ivec; c12 <- t(c21)
-                c22 <- C22
-
-                C <- rbind( cbind(c11,c12), cbind(c21, c22) )
-
-                D <- cbind(D1 %*% ivec, d2)
-
-
-                e12 <- (D1 %*% ivec) + d2; e21 <- t(e12)
-                e22 <- (t(ivec) %*% C11 %*% ivec) + (t(ivec) %*% C12) + (C21 %*% ivec) + C22
-                E.star <- rbind( cbind(Q22, e12), cbind(e21, e22) )
- 
-                E.star.inv <- inv.chol(E.star)
-                n.free <- length(f.idx); n.ref <- length(theta2.idx)
-                E11 <- E.star.inv[1:n.free, 1:n.free]
-                E21 <- t(E.star.inv[n.free + 1:n.ref, 1:n.free]); E12 <- t(E21)
-                E22 <- as.numeric(E.star.inv[n.free + 1:n.ref, n.free + 1:n.ref])
-                E.inv <- E11 - (E12 %*% E21)/E22
-
-                H <- C - (t(D) %*% E.inv %*% D)
-                h11 <- H[1,1]; h12 <- H[1,2]; h21 <- H[2,1]; h22 <- H[2,2]
-
-                mi[theta2.idx] <- dx[theta2.idx]*dx[theta2.idx] * (h11 + 2*h21 + h22) / (h11*h22 - h12*h12)
-            } #i
-        } # ncon
-    }
+    #if(length(model.idx) > 0L && object@Model@eq.constraints) {
+    #    # take care of equality constraints
+    #    # Sorbom 1989 equations 12 and 13
+    #    # surely this code needs some serious optimization
+    #    # but it seems to work for now
+    #    # we should use the K matrix somewhere...
+    #
+    #    A <- object@Model@ceq.JAC
+    #    ncon <- nrow(A) # # number of constraints
+    #
+    #    for(con in seq_len(ncon)) {
+    #
+    #        # what are the involved parameters (in model.idx)?
+    #        eq.idx <- which(A[con,] != 0)
+    #
+    #        for(i in 1:length(eq.idx)) {
+    #            # index in all.idx (only mi.fixed elements)
+    #            theta2.idx <- model.idx[ eq.idx[i]  ]
+    #            thetac.idx <- model.idx[ eq.idx[-i] ]
+    #            #f.idx <- free.idx[-which(free.idx == theta2.idx)]
+    #            f.idx <- model.idx[ -eq.idx ]
+    #            Q22 <- Q[f.idx, f.idx, drop=FALSE]
+    #
+    #            ivec <- as.matrix(rep(1,length(thetac.idx)))
+    #            C11 <- Q[thetac.idx, thetac.idx, drop=FALSE]
+    #            C21 <- Q[theta2.idx, thetac.idx, drop=FALSE]; C12 <- t(C21)
+    #            C22 <- Q[theta2.idx, theta2.idx, drop=FALSE]
+    #            D1  <- Q[f.idx, thetac.idx, drop=FALSE];
+    #            d2  <- Q[f.idx, theta2.idx, drop=FALSE]
+    # 
+    #            c11 <- t(ivec) %*% C11 %*% ivec
+    #            c21 <- C21 %*% ivec; c12 <- t(c21)
+    #            c22 <- C22
+    # 
+    #            C <- rbind( cbind(c11,c12), cbind(c21, c22) )
+    #
+    #            D <- cbind(D1 %*% ivec, d2)
+    #
+    #
+    #            e12 <- (D1 %*% ivec) + d2; e21 <- t(e12)
+    #            e22 <- (t(ivec) %*% C11 %*% ivec) + (t(ivec) %*% C12) + (C21 %*% ivec) + C22
+    #            E.star <- rbind( cbind(Q22, e12), cbind(e21, e22) )
+    # 
+    #            E.star.inv <- inv.chol(E.star)
+    #            #E.star.inv <- 
+    #            #    lav_model_information_augment_invert(object@Model, 
+    #            #        information = E.star, inverted = TRUE)
+    #            n.free <- length(f.idx); n.ref <- length(theta2.idx)
+    #            E11 <- E.star.inv[1:n.free, 1:n.free]
+    #            E21 <- t(E.star.inv[n.free + 1:n.ref, 1:n.free]); E12 <- t(E21)
+    #            E22 <- as.numeric(E.star.inv[n.free + 1:n.ref, n.free + 1:n.ref])
+    #            E.inv <- E11 - (E12 %*% E21)/E22
+    #
+    #            H <- C - (t(D) %*% E.inv %*% D)
+    #            h11 <- H[1,1]; h12 <- H[1,2]; h21 <- H[2,1]; h22 <- H[2,2]
+    #
+    #            mi[theta2.idx] <- dx[theta2.idx]*dx[theta2.idx] * (h11 + 2*h21 + h22) / (h11*h22 - h12*h12)
+    #        } #i
+    #    } # ncon
+    #}
 
     # EPC
     d <- (-1 * object@SampleStats@ntotal) * dx
@@ -299,26 +301,26 @@ modindices <- function(object,
     class(LIST) <- c("lavaan.data.frame", "data.frame")
 
     # add eq constraints (if any)
-    eq.idx <- which(partable$op == "==")
-    if(length(eq.idx) > 0L) {
-        N <- length(eq.idx)
-        TMP <- data.frame(lhs = partable$lhs[eq.idx],
-                           op = partable$op[eq.idx],
-                          rhs = partable$rhs[eq.idx],
-                          group = partable$group[eq.idx],
-                          mi = rep(as.numeric(NA), N),
-                          epc = rep(as.numeric(NA), N),
-                          sepc.lv = rep(as.numeric(NA), N),
-                          sepc.all = rep(as.numeric(NA), N),
-                          sepc.nox = rep(as.numeric(NA), N) )
-        if(!standardized) {
-            TMP$sepc.lv <- TMP$sepc.all <- TMP$sepc.nox <- NULL
-        }
-        if(!is.null(LIST$mi.scaled)) {
-            TMP$mi.scaled <- rep(as.numeric(NA), N)
-        }
-        LIST <- rbind(LIST, TMP)
-    }
+    #eq.idx <- which(partable$op == "==")
+    #if(length(eq.idx) > 0L) {
+    #    N <- length(eq.idx)
+    #    TMP <- data.frame(lhs = partable$lhs[eq.idx],
+    #                       op = partable$op[eq.idx],
+    #                      rhs = partable$rhs[eq.idx],
+    #                      group = partable$group[eq.idx],
+    #                      mi = rep(as.numeric(NA), N),
+    #                      epc = rep(as.numeric(NA), N),
+    #                      sepc.lv = rep(as.numeric(NA), N),
+    #                      sepc.all = rep(as.numeric(NA), N),
+    #                      sepc.nox = rep(as.numeric(NA), N) )
+    #    if(!standardized) {
+    #        TMP$sepc.lv <- TMP$sepc.all <- TMP$sepc.nox <- NULL
+    #    }
+    #    if(!is.null(LIST$mi.scaled)) {
+    #        TMP$mi.scaled <- rep(as.numeric(NA), N)
+    #    }
+    #    LIST <- rbind(LIST, TMP)
+    #}
 
     # remove 'existing' parameters
     #TMP1 <- data.frame(lhs = partable$lhs,
