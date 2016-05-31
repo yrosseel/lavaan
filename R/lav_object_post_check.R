d8220f8cb44c6aadbb330139e8c3e260a75ee02c
Yves Rosseel <yrosseel@gmail.com>
2016-05-31 19:44:30 +0200

    if negative variances are found, give a warning, but do not give a second (redundant) warning saying that the psi/theta matrix is non positive definite

--- R/lav_object_post_check.R
+++ R/lav_object_post_check.R
@@ -7,44 +7,63 @@ lav_object_post_check <- function(object, verbose = FALSE) {
     lavdata        <- object@Data
     lavfit         <- object@Fit
 
-    result.ok <- TRUE
+    var.ov.ok <- var.lv.ok <- result.ok <- TRUE
 
-    # 1. check for Heywood cases, negative (residual) variances, ...
+    # 1a. check for negative variances ov
     var.idx <- which(lavpartable$op == "~~" &
-                     #!lavpartable$lhs %in% unlist(lavpta$vnames$ov.ord) &
+                     lavpartable$lhs %in% lavNames(object, "ov") &
                      lavpartable$lhs == lavpartable$rhs)
     if(length(var.idx) > 0L && any(lavfit@est[var.idx] < 0.0)) {
-        result.ok <- FALSE
-        warning("lavaan WARNING: some estimated variances are negative")
+        result.ok <- var.ov.ok <- FALSE
+        warning("lavaan WARNING: some estimated ov variances are negative")
     }
 
-    # 2. is cov.lv (PSI) positive definite?
-    if(length(lavNames(lavpartable, type="lv.regular")) > 0L) {
+    # 1b. check for negative variances lv
+    var.idx <- which(lavpartable$op == "~~" &
+                     lavpartable$lhs %in% lavNames(object, "lv") &
+                     lavpartable$lhs == lavpartable$rhs)
+    if(length(var.idx) > 0L && any(lavfit@est[var.idx] < 0.0)) {
+        result.ok <- var.lv.ok <- FALSE
+        warning("lavaan WARNING: some estimated lv variances are negative")
+    }
+
+    # 2. is cov.lv (PSI) positive definite? (only if we did not already warn
+    # for negative variances)
+    if(var.lv.ok && length(lavNames(lavpartable, type="lv.regular")) > 0L) {
         ETA <- lavTech(object, "cov.lv")
         for(g in 1:lavdata@ngroups) {
             txt.group <- ifelse(lavdata@ngroups > 1L,
-                                paste("in group", g, ".", sep=""), "")
+                                paste(" in group ", g, sep=""), "")
             eigvals <- eigen(ETA[[g]], symmetric=TRUE, only.values=TRUE)$values
             if(any(eigvals < -1 * .Machine$double.eps^(3/4))) {
-                warning("lavaan WARNING: covariance matrix of latent variables is not positive definite;", txt.group, " use inspect(fit,\"cov.lv\") to investigate.")
+                warning(
+"lavaan WARNING: covariance matrix of latent variables\n",
+"                is not positive definite", txt.group, ";\n", 
+"                use inspect(fit,\"cov.lv\") to investigate.")
                 result.ok <- FALSE
             }
         }
     }
 
     # 3. is THETA positive definite (but only for numeric variables)
-    THETA <- lavTech(object, "theta")
-    for(g in 1:lavdata@ngroups) {
-        num.idx <- lavmodel@num.idx[[g]]
-        if(length(num.idx) > 0L) {
-            txt.group <- ifelse(lavdata@ngroups > 1L,
-                                paste("in group", g, ".", sep=""), "")
-            eigvals <- eigen(THETA[[g]][num.idx, num.idx, drop=FALSE],
-                             symmetric = TRUE,
-                             only.values = TRUE)$values
-            if(any(eigvals < -1 * .Machine$double.eps^(3/4))) {
-                warning("lavaan WARNING: observed variable error term matrix (theta) is not positive definite;", txt.group, " use inspect(fit,\"theta\") to investigate.")
-                result.ok <- FALSE
+    # and if we not already warned for negative ov variances
+    if(var.ov.ok) {
+        THETA <- lavTech(object, "theta")
+        for(g in 1:lavdata@ngroups) {
+            num.idx <- lavmodel@num.idx[[g]]
+            if(length(num.idx) > 0L) {
+                txt.group <- ifelse(lavdata@ngroups > 1L,
+                                    paste(" in group ", g, sep=""), "")
+                eigvals <- eigen(THETA[[g]][num.idx, num.idx, drop=FALSE],
+                                 symmetric = TRUE,
+                                 only.values = TRUE)$values
+                if(any(eigvals < -1 * .Machine$double.eps^(3/4))) {
+                    warning(
+"lavaan WARNING: the covariance matrix of the residuals of the observed\n", 
+"                variables (theta) is not positive definite", txt.group, ";\n",
+"                use inspect(fit,\"theta\") to investigate.")
+                    result.ok <- FALSE
+                }
             }
         }
     }
