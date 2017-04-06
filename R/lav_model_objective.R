3c91ffb580601f762be66282e7d27436d1ce86c2
ecmerkle <merklee@missouri.edu>
2017-04-05 21:50:00 -0500

    avoid PML warnings when there are multiple groups

--- R/lav_model_objective.R
+++ R/lav_model_objective.R
@@ -77,7 +77,7 @@ lav_model_objective <- function(lavmodel       = NULL,
             PI <- computePI(lavmodel = lavmodel, GLIST = GLIST)
         }
         if(estimator == "PML") {
-            if(lavmodel@nexo > 0L) {
+            if(any(lavmodel@nexo > 0L)) {
                 PI <- computePI(lavmodel = lavmodel)
             } else {
                 PI <- vector("list", length = lavsamplestats@ngroups)
