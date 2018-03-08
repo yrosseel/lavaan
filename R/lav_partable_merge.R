5dc736317f9dcc7f4a0e4ad71d74ca8b40b3971f
TDJorgensen <TJorgensen314@gmail.com>
2018-03-08 21:34:17 +0100

    fix printing of duplicated parameters

--- R/lav_partable_merge.R
+++ R/lav_partable_merge.R
@@ -111,7 +111,7 @@ lav_partable_merge <- function(pt1 = NULL, pt2 = NULL,
         if(length(idx)) {
             if(warn) {
                 warning("lavaan WARNING: duplicated parameters are ignored:\n",
-                paste(apply(pt1[idx, c("lhs","op","rhs")], 1,
+                paste(apply(TMP[idx, c("lhs","op","rhs")], 1,
                       paste, collapse=" "), collapse="\n"))
             }
             if(fromLast) {
