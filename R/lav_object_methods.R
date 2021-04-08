a528028973c9b9ffc4afbde3c4a663a9e9a9be23
Aaron Peikert <aaron.peikert@posteo.de>
2021-04-08 10:48:02 +0200

    add labels to standardizedSolution() closes #204

--- R/lav_object_methods.R
+++ R/lav_object_methods.R
@@ -163,6 +163,9 @@ standardizedSolution <-
     if(!is.null(PARTABLE$block)) {
         LIST$block <- PARTABLE$block
     }
+    if (sum(nchar(PARTABLE$label)) != 0L) {
+      LIST$label <- PARTABLE$label
+    }
 
     # add std and std.all columns
     if(type == "std.lv") {
