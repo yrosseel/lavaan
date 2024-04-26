c461755ff58e0ced9fcdb10a60937c602893ba66
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-04-26 14:58:52 +0800

    Simple script to verify results

--- experiments/haziq_cluster_se.R
+++ experiments/haziq_cluster_se.R
@@ -1,87 +1,14 @@
+# pak::pak("haziqj/lavaan.bingof")
 library(lavaan.bingof)
 library(tidyverse)
-
-dat <- gen_data_bin_clust(n = 1000)
-fit <- sem(
-  model = txt_mod(1), 
-  data = dat, 
-  estimator = "PML",
-  std.lv = TRUE,
-  sampling.weights = "wt"
-  # cluster = "school"
-)
-
-# Normal non-clustered, but possibly weighted SE -------------------------------
-Delta <- computeDelta(lavmodel = fit@Model)[[1]]
-SC <- pml_deriv1(
-  Sigma.hat = fit@implied$cov[[1]],
-  Mu.hat = fit@implied$mean[[1]],
-  TH = fit@implied$th[[1]],
-  th.idx = fit@Model@th.idx[[1]],
-  num.idx = fit@Model@num.idx[[1]],
-  X = fit@Data@X[[1]],
-  eXo = NULL,
-  wt = NULL,
-  PI = NULL,
-  lavcache = fit@Cache[[1]],
-  missing = fit@Data@missing,
-  scores = TRUE,
-  negative = FALSE
-)
-B1 <- crossprod(SC)
-J <- t(Delta) %*% B1 %*% Delta / nrow(dat)
-Hinv <- lavaan.bingof:::get_sensitivity_inv_mat(fit)
-G <- Hinv %*% J %*% Hinv
-
-sqrt(diag(G) / nrow(dat)) 
-partable(fit)$se[partable(fit)$free != 0]
-
-# Clustered SE -----------------------------------------------------------------
-SCwt <-
-  dat |>
-  select(school, wt) |>
-  bind_cols(as_tibble(SC))
-
-clusters <- unique(SCwt$school)
-nclust <- length(clusters)
-zb <- list()
-for (b in seq_along(clusters)) {
-  dat_b <- 
-    SCwt |>
-    filter(school == clusters[b])
-  
-  tmp <- select(dat_b, -school, -wt) * dat_b$wt
-  tmp <- apply(tmp, 2, sum)
-  zb[[b]] <- tmp
-}
-zbar <- apply(do.call(cbind, zb), 1, mean)
-
-B1c <-
-  lapply(zb, \(z) tcrossprod(z - zbar)) |>
-  Reduce(f = `+`)
-Jc <- nclust / (nclust - 1) * (t(Delta) %*% B1c %*% Delta) / nrow(dat)
-Gc <- Hinv %*% Jc %*% Hinv
-
-sqrt(diag(Gc) / nrow(dat)) 
-partable(fit)$se[partable(fit)$free != 0]
-
-# Export to ask Irini ----------------------------------------------------------
-# SC_ <- SC %*% Delta
-# colnames(SC_) <- names(get_true_values(1))
-# bind_cols(
-#   h = 1:nrow(dat),
-#   select(dat, school, wt),
-#   as_tibble(SC_)
-# ) |>
-#   write_csv("SC.csv")
 
 # Function to obtain clustered SE ----------------------------------------------
 get_clust_se <- function(object, dat) {
   
   # Obtain scores
   Hinv <- lavaan.bingof:::get_sensitivity_inv_mat(object)  # H^{-1}
-  Delta <- computeDelta(lavmodel = object@Model)[[1]]
-  SC <- pml_deriv1(
+  Delta <- lavaan:::computeDelta(lavmodel = object@Model)[[1]]
+  SC <- lavaan:::pml_deriv1(
     Sigma.hat = object@implied$cov[[1]],
     Mu.hat = object@implied$mean[[1]],
     TH = object@implied$th[[1]],
@@ -130,7 +57,7 @@ get_clust_se <- function(object, dat) {
   out
 }
 
-# test function
+# Test the function and compare with lavaan ------------------------------------
 mod_no <- 5
 dat <- gen_data_bin_clust(population = make_population(mod_no), n = 1000)
 fit <- sem(
@@ -141,4 +68,21 @@ fit <- sem(
   sampling.weights = "wt"
   # cluster = "school"
 )
-get_clust_se(fit, dat)
+se_manual <- get_clust_se(fit, dat)
+
+fitc <- sem(
+  model = txt_mod(mod_no), 
+  data = dat, 
+  estimator = "PML",
+  std.lv = TRUE,
+  sampling.weights = "wt",
+  cluster = "school"
+)
+se_lav <- partable(fitc)$se[partable(fitc)$free != 0]
+
+# Compare
+sqrt(sum((se_manual - se_lav) ^ 2))  # should be zero
+
+plot(se_lav, se_manual)
+abline(a = 0, b = 1, col = "red")
+
